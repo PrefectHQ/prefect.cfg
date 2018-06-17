3499aa2a2e88a783a5bd1c5814b6d04a31935ba7
Jeremiah Lowin <jlowin@users.noreply.github.com>
2018-06-17 16:55:04 -0400

    Add fonts

---
+++ fonts.sh
@@ -0,0 +1,25 @@
+echo "\n--------------------------------------------------"
+echo "-> Preparing to install fonts..."
+echo "--------------------------------------------------\n"
+
+brew tap homebrew/cask-fonts
+
+echo "\n--------------------------------------------------"
+echo "-> Installing Input..."
+echo "--------------------------------------------------\n"
+brew cask install font-input
+
+echo "\n--------------------------------------------------"
+echo "-> Installing Source Code Pro..."
+echo "--------------------------------------------------\n"
+brew cask install font-source-code-pro
+
+echo "\n--------------------------------------------------"
+echo "-> Installing Anonymous Pro..."
+echo "--------------------------------------------------\n"
+brew cask install font-anonymous-pro
+
+echo "\n--------------------------------------------------"
+echo "-> Installing Fira Code..."
+echo "--------------------------------------------------\n"
+brew cask install font-fira-code
