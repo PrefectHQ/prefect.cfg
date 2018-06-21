2ad893ee4d57e25a6f4c6e98c17f55e733464290
Jeremiah Lowin <jlowin@users.noreply.github.com>
2018-06-21 08:32:21 -0400

    Add node

---
+++ node.sh
@@ -0,0 +1,7 @@
+# Anaconda
+echo "--------------------------------------------------"
+echo "-> Installing Node..."
+echo "-> https://nodejs.org/"
+echo "--------------------------------------------------\n"
+brew install node
+brew postinstall node
