c8d0ad2f250370c166658a5d18019fcf38c443bc
Jeremiah Lowin <153965+jlowin@users.noreply.github.com>
2018-11-11 09:45:31 -0500

    Update node.sh

--- node.sh
+++ node.sh
@@ -1,7 +1,21 @@
-# Anaconda
+# Node
 echo "--------------------------------------------------"
 echo "-> Installing Node..."
 echo "-> https://nodejs.org/"
 echo "--------------------------------------------------\n"
 brew install node
 brew postinstall node
+
+# Yarn
+echo "--------------------------------------------------"
+echo "-> Installing Yarn..."
+echo "-> https://yarnpkg.com/"
+echo "--------------------------------------------------\n"
+brew install yarn
+
+# Prettier
+echo "--------------------------------------------------"
+echo "-> Installing Prettier..."
+echo "-> https://prettier.io/"
+echo "--------------------------------------------------\n"
+yarn global add prettier
