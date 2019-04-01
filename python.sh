ed8a4f8fbd025a62fd5e109b1ea4a721cd381738
Tyler Wanner <tyler@prefect.io>
2019-04-01 17:14:20 -0400

    fixed anaconda install

--- python.sh
+++ python.sh
@@ -3,7 +3,11 @@ echo "--------------------------------------------------"
 echo "-> Installing Anaconda..."
 echo "-> https://www.anaconda.com/distribution/"
 echo "--------------------------------------------------\n"
-brew cask install anaconda
+#brew cask install anaconda
+if [[ -z `which conda` ]]; then
+curl -fLO https://repo.continuum.io/archive/Anaconda3-2018.12-MacOSX-x86_64.sh
+bash Anaconda3-2018.12-MacOSX-x86_64.sh
+fi
 
 echo "\n--------------------------------------------------"
 echo "-> Installing Python packages..."
