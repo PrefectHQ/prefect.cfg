a656cdd9d1b06a8f686d3f48719dfdfc794c962f
Tyler Wanner <tyler@prefect.io>
2019-04-01 17:23:40 -0400

    for easier version changes

--- python.sh
+++ python.sh
@@ -4,9 +4,10 @@ echo "-> Installing Anaconda..."
 echo "-> https://www.anaconda.com/distribution/"
 echo "--------------------------------------------------\n"
 #brew cask install anaconda
+ANACONDA_VERSION=Anaconda3-2018.12-MacOSX-x86_64
 if [[ -z `which conda` ]]; then
-curl -fLO https://repo.continuum.io/archive/Anaconda3-2018.12-MacOSX-x86_64.sh
-bash Anaconda3-2018.12-MacOSX-x86_64.sh
+curl -fLO https://repo.continuum.io/archive/$ANACONDA_VERSION.sh
+bash $ANACONDA_VERSION.sh
 fi
 
 echo "\n--------------------------------------------------"
